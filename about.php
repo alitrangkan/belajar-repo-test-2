aca3851a53536100a3a623c345a6c25ddb944644
alitrangkan <rangkan.nuaja@gmail.com>
2022-10-25 12:03:49 +0800

    Menambahkan file about.php

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Tittel</title>
+</head>
+<body>
+    <h1>Tetang Program Magang</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint sed voluptate quod iure eaque quia necessitatibus doloremque, dolorum soluta provident totam facilis sequi dolore! A natus laborum modi reprehenderit cupiditate voluptates unde repudiandae ad, facere veniam quo consequatur quam possimus ipsum odio voluptas fugit recusandae voluptatem assumenda debitis harum doloremque ipsam magnam. Harum perspiciatis eum, optio, vitae ut corporis atque vel laudantium blanditiis neque fugiat perferendis. Error eos dolore nam magnam omnis enim odit accusamus veniam. Dolores consequatur labore autem, maxime nobis enim assumenda dolorem blanditiis, dolorum optio nesciunt quia officia quas eum, odit sapiente? Sed nesciunt earum autem corporis?</p>
+</body>
+</html>
